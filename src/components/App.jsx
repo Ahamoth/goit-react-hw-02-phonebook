99e7c278e10d93f254730685659d9bc283a87778
Ahamoth <beeminer.store@gmail.com>
2023-02-26 20:39:55 +0200

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './Form/Form';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    if (this.state.contacts.find(contact => contact.name === newContact.name)) {
+      alert(`${newContact.name} is already in contacts.`);
+      return '';
+    }
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+  handleFilter = text => {
+    this.setState({ filter: text });
+  };
+
+  onDeleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    return (
+      <div className="wrapper">
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter onChange={this.handleFilter} value={filter} />
+        <ContactList
+          contacts={contacts}
+          filter={filter}
+          onDeleteContact={this.onDeleteContact}
+        />
+      </div>
+    );
+  }
+}
